13bb186bf2b0f74eb2fc07bdd9f1303b2d27b70e
Abhinav <abhiraja1012@gmail.com>
2019-12-19 22:12:37 -0500

    link tmux conf

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -1,2 +1,5 @@
 # to edit the actual dotfiles in ~/ to read from this dotfiles repo
 echo "source ~/.dotfiles/zsh/.zshrc" > ~/.zshrc
+echo "source ~/.dotfiles/bash/.bashrc" > ~/.bashrc
+echo "source ~/.dotfiles/tmux/.tmux.conf" > ~/.tmux.conf
+
